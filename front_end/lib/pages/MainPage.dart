f9c4c6bfdef61b373f6d2693439463f27cfc6341
Bae Hyeonji <wns038200@naver.com>
2025-03-30 03:07:57 +0900

    feat: 초기세팅

---
+++ front_end/lib/pages/MainPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("홈")),
+      body: Center(child: const Text("홈 화면")),
+    );
+  }
+}
---
+++ front_end/lib/pages/survey/SurveyPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SurveyPage extends StatefulWidget {
+  const SurveyPage({super.key});
+
+  @override
+  State<SurveyPage> createState() => _SurveyPageState();
+}
+
+class _SurveyPageState extends State<SurveyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
