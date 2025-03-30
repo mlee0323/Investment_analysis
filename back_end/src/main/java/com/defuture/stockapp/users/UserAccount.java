a01d373727cd111d20bb3c6b9a0295cc6d22f269
juno801 <juno306@daum.net>
2025-03-30 18:57:39 +0900

    Implement investment profile feature
    Implemented the investment profile functionality including:
    - Calculating the total score based on user responses.
    - Determining the investment type.
    - Updating the user account with the calculated score and type.
    
    사용자 응답을 기반으로 총점을 계산하고, 투자 성향을 결정하여
    사용자 계정을 업데이트하는 기능을 구현하였습니다.

--- back_end/src/main/java/com/defuture/stockapp/users/UserAccount.java
+++ back_end/src/main/java/com/defuture/stockapp/users/UserAccount.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.persistence.Id;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,13 +12,19 @@ import lombok.Setter;
 @Document(collection = "user_accounts")
 public class UserAccount {
 	@Id
-	private String username;
-	
-	private String name;
+	private String id;
 	
 	@Indexed(unique = true)
+	private String username;
+
+	private String name;
+
+	@Indexed(unique = true)
 	private String email;
-	
+
 	private String password;
-	
+
+	private String investmentType;
+	private Double investmentScore;
+
 }
