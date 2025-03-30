d32025a643b128d7bdcf65d52d5b63d9473c4ee8
Bae Hyeonji <wns038200@naver.com>
2025-03-30 20:53:37 +0900

    fix: 초기세팅 수정

---
+++ front_end/lib/pages/auth/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
