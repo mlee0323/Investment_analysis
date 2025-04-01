f931b055a4706269ee7ad525cf6f52cbbd2626f6
Bae Hyeonji <wns038200@naver.com>
2025-04-01 16:23:22 +0900

    feat: screens ui

---
+++ front_end/lib/screens/mydata_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MydataScreen extends StatefulWidget {
+  const MydataScreen({super.key});
+
+  @override
+  State<MydataScreen> createState() => _MyDataLoaderPageState();
+}
+
+class _MyDataLoaderPageState extends State<MydataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ front_end/lib/widgets/custom_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final isFullWidth;
+  final color;
+  final backgroundColor;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.isFullWidth,
+    this.color = Colors.white,
+    this.backgroundColor = Colors.blueAccent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: isFullWidth == true ? double.infinity : null,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          foregroundColor: color,
+          backgroundColor: backgroundColor,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          textStyle: const TextStyle(fontSize: 16),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
