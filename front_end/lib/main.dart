767e7aafba53d9667806fd8a53b4e1c36ef7577e
Bae Hyeonji <wns038200@naver.com>
2025-04-01 16:24:12 +0900

    feat: user provider

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
-import 'pages/home_screen.dart';
+import 'package:front_end/provider/user_provider.dart';
+import 'package:front_end/screens/auth/login_screen.dart';
+import 'package:front_end/screens/auth/signin_screen.dart';
+import 'package:front_end/screens/market_screen.dart';
+import 'package:front_end/screens/portfolio_screen.dart';
+import 'package:front_end/screens/setting_screen.dart';
+import 'screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +28,47 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      initialRoute: "/",
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return PageRouteBuilder(
+              pageBuilder:
+                  (context, animation, secondaryAnimation) => HomeScreen(),
+              transitionDuration: Duration(seconds: 0),
+            );
+          case '/signin':
+            return PageRouteBuilder(
+              pageBuilder:
+                  (context, animation, secondaryAnimation) => SigninScreen(),
+              transitionDuration: Duration(seconds: 0),
+            );
+          case '/login':
+            return PageRouteBuilder(
+              pageBuilder:
+                  (context, animation, secondaryAnimation) => LoginScreen(),
+              transitionDuration: Duration(seconds: 0),
+            );
+          case '/portfolio':
+            return PageRouteBuilder(
+              pageBuilder:
+                  (context, animation, secondaryAnimation) => PortfolioScreen(),
+              transitionDuration: Duration(seconds: 0),
+            );
+          case '/market':
+            return PageRouteBuilder(
+              pageBuilder:
+                  (context, animation, secondaryAnimation) => MarketScreen(),
+              transitionDuration: Duration(seconds: 0),
+            );
+          case '/setting':
+            return PageRouteBuilder(
+              pageBuilder:
+                  (context, animation, secondaryAnimation) => SettingScreen(),
+              transitionDuration: Duration(seconds: 0),
+            );
+        }
+      },
     );
   }
 }
