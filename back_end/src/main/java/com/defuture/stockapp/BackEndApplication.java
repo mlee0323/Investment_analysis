d5c6088d513fa2ffcb03227ec00ca3421e131d91
juno801 <juno306@daum.net>
2025-04-05 23:46:25 +0900

    chore: minor config and docs updates
    - Cleaned up .gitignore\n- Improved formatting of API response in README.md\n- Added startup settings to BackEndApplication.java\n\n.gitignore 정리, README 예시 정리, BackEndApplication.java에 설정 추가

--- back_end/src/main/java/com/defuture/stockapp/BackEndApplication.java
+++ back_end/src/main/java/com/defuture/stockapp/BackEndApplication.java
@@ -3,7 +3,12 @@ package com.defuture.stockapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+	    exclude = {
+	            org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+	            org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+	        }
+	    )
 public class BackEndApplication {
 
 	public static void main(String[] args) {
